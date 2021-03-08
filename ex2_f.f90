23f4fda0500f5662967afb72114c140d6c7cd675
sexyryan <46165966+sexyryan@users.noreply.github.com>
2021-03-08 02:07:48 -0800

    Update ex2_f.f90

--- ex2_f.f90
+++ ex2_f.f90
@@ -1,16 +1,16 @@
 program FFT_MPI_3D
        use, intrinsic :: iso_c_binding
        implicit none
- include 'mpif.h'
+       include 'mpif.h'
        include 'fftw3-mpi.f03'
 
        ! Parameter Definition
-       integer(C_INTPTR_T), parameter :: L = 1024
-       integer(C_INTPTR_T), parameter :: M = 1024
+       INTEGER(C_INTPTR_T), parameter :: L = 1024
+       INTEGER(C_INTPTR_T), parameter :: M = 1024
 
        !Variable Definition
-       integer(C_INTPTR_T) :: alloc_local, local_M, local_j_offset
-       integer(C_INTPTR_T) :: i, j
+       INTEGER(C_INTPTR_T) :: alloc_local, local_M, local_j_offset
+       INTEGER(C_INTPTR_T) :: i, j
 
        complex(C_DOUBLE_COMPLEX), pointer :: fdata(:,:)
        complex(C_DOUBLE_COMPLEX) :: fout
@@ -20,7 +20,7 @@ program FFT_MPI_3D
 
        integer :: ierr, myid, nproc
 
-       real(C_DOUBLE) :: t1, t2, t3, t4, tplan, texec
+       INTEGER(C_DOUBLE) :: t1, t2, t3, t4, tplan, tmid, texec
 !
 ! Initialize
    call mpi_init(ierr)
@@ -37,7 +37,7 @@ program FFT_MPI_3D
 !   create MPI plan for in-place forward DFT (note dimension reversal)
          t1 = MPI_wtime()
    plan = fftw_mpi_plan_dft_2d(M, L, fdata, fdata, &
-    &         MPI_COMM_WORLD, FFTW_FORWARD, FFTW_MEASURE)
+    &         MPI_COMM_WORLD, FFTW_FORWARD, FFTW_ESTIMATE)
          t2 = MPI_wtime()
 !
 ! initialize data to some function my_function(i,j)
@@ -54,9 +54,10 @@ program FFT_MPI_3D
          t4 = MPI_wtime()
 !
 ! print solutinos
-         tplan = t2-t1
-         texec =t4-t3
-         if (myid.eq.0) print*,'Tplan=',tplan,'   Texec=',texec
+         tplan = t2 - t1
+         tmid = t3 - t2
+         texec = t4 - t3
+         if (myid.eq.0) print*,'Tplan=',tplan,'   Tmid=',tmid,'   Texec=',texec
 !
 ! deallocate and destroy plans
    call fftw_destroy_plan(plan)
@@ -72,19 +73,19 @@ program FFT_MPI_3D
 use, intrinsic :: iso_c_binding
  integer(C_INTPTR_T), intent(in) :: i, j, L, M
  complex(C_DOUBLE_COMPLEX), intent(out) :: fout
- real(C_DOUBLE), parameter :: amp = 0.25
- real(C_DOUBLE) :: xx, yy, LL, MM, r1
+ INTEGER(C_DOUBLE), parameter :: amp = 0.25
+ INTEGER(C_DOUBLE) :: xx, yy, LL, MM, r1
 
-   xx = real(i, C_DOUBLE) - real((L+1)/2, C_DOUBLE)
-   yy = real(j, C_DOUBLE) - real((M+1)/2, C_DOUBLE)
-   LL = real(L, C_DOUBLE)
-   MM = real(M, C_DOUBLE)
+   xx = INTEGER(i, C_DOUBLE) - INTEGER((L+1)/2, C_DOUBLE)
+   yy = INTEGER(j, C_DOUBLE) - INTEGER((M+1)/2, C_DOUBLE)
+   LL = INTEGER(L, C_DOUBLE)
+   MM = INTEGER(M, C_DOUBLE)
 
    r1 = sqrt(((xx/LL)**2.) + ((yy/MM)**2.))
    if (r1 .le. amp) then
      fout = CMPLX(1., 0. , C_DOUBLE_COMPLEX)
          else
-           fout = CMPLX(0., 0. , C_DOUBLE_COMPLEX)
+           fout = CMPLX(0., 1. , C_DOUBLE_COMPLEX)
          endif
 
      return
@@ -92,4 +93,4 @@ use, intrinsic :: iso_c_binding
 ! **********************************************************************
 
 !to compile
-!mpif90 -I/usr/local/include -L/usr/local/lib -o test ex2_f.f90 -lfftw3_mpi -lfftw3 -lm
+!mpif90 -I/usr/local/include -L/usr/local/lib -o ex2_f ex2_f.f90 -lfftw3_mpi -lfftw3 -lm
